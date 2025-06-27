6059c1b00ed9a8a8cdbdefbcd64f765f14748894
rpourteau <remy.pourteau@cosylab.com>
2025-06-27 15:28:52 +0200

    Improve eshell and update gitignore

--- config/linuxos.el
+++ config/linuxos.el
@@ -22,3 +22,20 @@
   (ansi-term (getenv "SHELL")))
 
 (global-set-key (kbd "C-c t") #'my-term)
+
+;;**********************************************************************
+;; ansi-term
+;;**********************************************************************
+(defun eshell-load-bash-aliases ()
+  "Read Bash aliases and add them to the list of eshell aliases."
+  ;; Bash needs to be run - temporarily - interactively
+  ;; in order to get the list of aliases.
+    (with-temp-buffer
+      (call-process "bash" nil '(t nil) nil "-ci" "alias")
+      (goto-char (point-min))
+      (while (re-search-forward "alias \\(.+\\)='\\(.+\\)'$" nil t)
+        (eshell/alias (match-string 1) (match-string 2)))))
+
+;; We only want Bash aliases to be loaded when Eshell loads its own aliases,
+;; rather than every time `eshell-mode' is enabled.
+(add-hook 'eshell-alias-load-hook 'eshell-load-bash-aliases)
