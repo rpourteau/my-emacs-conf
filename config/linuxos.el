f5486997378300205023355beb01f20cae800751
rpourteau <remy.pourteau@cosylab.com>
2025-06-27 15:23:41 +0200

    Add keybind to ainsi-term

--- config/linuxos.el
+++ config/linuxos.el
@@ -1,8 +1,24 @@
+;;**********************************************************************
+;; linuxos.el -- Set-up of tools that only work on Linux OS
+;;**********************************************************************
 
-; magit
+;;**********************************************************************
+;; magit
+;;**********************************************************************
 (use-package magit
   :straight t
   :commands (magit-status magit-blame)
   :bind (("C-x g" . magit-status))
   :config
   (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1))
+
+;;**********************************************************************
+;; ansi-term
+;;**********************************************************************
+;; Better use ansi-term rather than shell, key bind to C-c t
+(defun my-term ()
+  "Launch ansi-term with the user's shell."
+  (interactive)
+  (ansi-term (getenv "SHELL")))
+
+(global-set-key (kbd "C-c t") #'my-term)
